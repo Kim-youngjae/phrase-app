caf822147176a32e31c238ffbd74a17d3d78f1dd
youngjae459 <youngjae459@gmail.com>
2023-03-02 09:41:03 +0900

    2단계완료

--- src/main/java/com/myphrase/Main.java
+++ src/main/java/com/myphrase/Main.java
@@ -4,10 +4,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+    }
+}
+
+class App {
+    static void run() {
         Scanner sc = new Scanner(System.in);
 
+        // 명언 앱 시작
         System.out.println("== 명언 앱 ==");
-        System.out.print("명령) ");
-        sc.next();
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = sc.next(); // 명령 입력
+
+            if (cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                sc.next();
+                System.out.print("작가 : ");
+                sc.next();
+            } else if (cmd.equals("종료")) {
+                break;
+            }
+        }
     }
 }
