b228577807b84a9cf5008dfd7dc0c69b5a1b5f7e
youngjae459 <youngjae459@gmail.com>
2023-03-02 09:55:53 +0900

    3단계완료

--- src/main/java/com/myphrase/Main.java
+++ src/main/java/com/myphrase/Main.java
@@ -4,13 +4,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        new App().run();
     }
 }
 
 class App {
     static void run() {
         Scanner sc = new Scanner(System.in);
+        int id = 1;
 
         // 명언 앱 시작
         System.out.println("== 명언 앱 ==");
@@ -20,9 +21,12 @@ class App {
 
             if (cmd.equals("등록")) {
                 System.out.print("명언 : ");
-                sc.next();
+                String phrase = sc.next();
                 System.out.print("작가 : ");
-                sc.next();
+                String writer = sc.next();
+
+                System.out.printf("%d번 명령이 등록되었습니다.\n", id++);
+
             } else if (cmd.equals("종료")) {
                 break;
             }
